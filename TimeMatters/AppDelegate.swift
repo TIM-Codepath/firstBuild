3ca4fda685d2920487dddd462b55c90c35065f82
aryumjeon <zalwata@hotmail.com>
2021-04-10 22:33:56 -0700

    Parse[signUP] is completed/tested.

--- TimeMatters/AppDelegate.swift
+++ TimeMatters/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration
+        {
+            $0.applicationId = "mYlgWG6aQp0Ku5sy5joJEUgj4E4hqsmq0PmDR9Kz"
+            $0.clientKey = "fC64bfLbEY6mFMBg2Ob3t0wBcAuEygit1efsQblG"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
