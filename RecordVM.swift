ad73b6e54f0ce3930efa3735219efb85b8d3089a
Berk Burak Tasdemir <burakt546@live.com>
2021-04-15 16:50:53 -0700

    Implementing profile page & view models

---
+++ RecordVM.swift
@@ -0,0 +1,17 @@
+//
+//  RecordVM.swift
+//  TimeMatters
+//
+//  Created by Berk Burak Tasdemir on 4/15/21.
+//
+
+import Foundation
+
+struct RecordVM {
+    let author: UserVM
+    let objectId: String
+    let image: String
+    let description: String
+    let startedAt: Date
+    let endedAt: Date
+}
