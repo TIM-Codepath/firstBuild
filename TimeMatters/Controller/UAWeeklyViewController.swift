177fdc1ae2ed1cddfe08cf74cbfb40be4b5b9042
aryumjeon <zalwata@hotmail.com>
2021-04-18 12:00:43 -0700

    UA daily prototype confirmed

---
+++ TimeMatters/Controller/UAWeeklyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UAWeeklyViewController.swift
+//  TimeMatters
+//
+//  Created by Aryum Jeon on 4/18/21.
+//
+
+import UIKit
+
+class UAWeeklyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
