b91e368455b8999893ef5685c334441ca5c91cfa
par5ul1 <elexir0@gmail.com>
2021-04-21 22:58:44 -0700

    Visual improvements + a timer page with a working timer that records the time elapsed

---
+++ TimeMatters/Controller/TimeLoggingViewController.swift
@@ -0,0 +1,93 @@
+//
+//  TImerViewController.swift
+//  TimeMatters
+//
+//  Created by Parsa Rahimi on 4/21/21.
+//
+
+import UIKit
+
+class TimeLoggingViewController: UIViewController {
+    
+    @IBOutlet weak var hourLabel: UILabel!
+    @IBOutlet weak var minuteLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var startStopBtn: UIButton!
+    
+    var timer = Timer()
+    var isTimerActive = false;
+    var timeElapsed = 0
+    
+    var (hours, minutes, seconds) = (0,0,0)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        startStopBtn.layer.masksToBounds = true
+        startStopBtn.layer.cornerRadius = startStopBtn.bounds.width / 2
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onStartStop(_ sender: Any) {
+        if !isTimerActive {
+            start()
+            startStopBtn.backgroundColor = UIColor(red: 0.88, green: 0.24, blue: 0.18, alpha: 1.00)
+            startStopBtn.setTitle("Stop", for: .normal)
+            isTimerActive = true;
+        } else {
+            stop()
+            startStopBtn.backgroundColor = UIColor(red: 0.01, green: 0.71, blue: 0.36, alpha: 1.00)
+            startStopBtn.setTitle("Start", for: .normal)
+            isTimerActive = false;
+        }
+    }
+    
+    func start() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(TimeLoggingViewController.keepTimer), userInfo: nil, repeats: true)
+    }
+    
+    func stop() {
+        timer.invalidate()
+        (hours, minutes, seconds) = (0,0,0)
+        hourLabel.text = "00"
+        minuteLabel.text = "00"
+        secondLabel.text = "00"
+        
+        // Save the time elapsed to database
+        print(timeElapsed) // TEMP
+        timeElapsed = 0
+    }
+    
+    @objc func keepTimer() {
+        
+        seconds += 1
+        
+        if seconds == 60 {
+            minutes += 1
+            seconds = 0
+        }
+        
+        if minutes == 60 {
+            hours += 1
+            minutes = 0
+        }
+        
+        hourLabel.text = hours > 9 ? "\(hours)" : "0\(hours)"
+        minuteLabel.text = minutes > 9 ? "\(minutes)" : "0\(minutes)"
+        secondLabel.text = seconds > 9 ? "\(seconds)" : "0\(seconds)"
+        
+        timeElapsed += 1
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
