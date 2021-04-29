63d16a6d318aafee1ee527258abac2e7500335ed
Berk Burak Tasdemir <burakt546@live.com>
2021-04-29 03:59:53 -0700

    Implementing Calendar UI

---
+++ TimeMatters/Controller/DateCell.swift
@@ -0,0 +1,14 @@
+//
+//  DateCell.swift
+//  TimeMatters
+//
+//  Created by Berk Burak Tasdemir on 4/29/21.
+//
+
+import JTAppleCalendar
+import UIKit
+
+class DateCell: JTAppleCell {
+    @IBOutlet weak var dateLabel: UILabel!
+    
+}
