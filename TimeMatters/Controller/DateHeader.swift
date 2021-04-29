9bc4597d4f9a75b3ca40d58f91dfc6fcce47b83d
Berk Burak Tasdemir <burakt546@live.com>
2021-04-29 03:59:53 -0700

    Implementing Calendar UI

---
+++ TimeMatters/Controller/DateHeader.swift
@@ -0,0 +1,14 @@
+//
+//  DateHeader.swift
+//  TimeMatters
+//
+//  Created by Berk Burak Tasdemir on 4/29/21.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class DateHeader: JTAppleCollectionReusableView {
+    @IBOutlet weak var monthTitle: UILabel!
+    
+}
